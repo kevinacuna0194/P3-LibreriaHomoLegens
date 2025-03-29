9d075c547257c1919a69df185c4c5ed62760de2d
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-03-29 19:45:34 -0300

    Capa Lógica de Negocio. Implementar Excepciones Propias en Entidad de Dominio

---
+++ ExcepcionesPropias/DatosInvalidosException.cs
@@ -0,0 +1,12 @@
+﻿namespace ExcepcionesPropias
+{
+    public class DatosInvalidosException : Exception
+    {
+        public DatosInvalidosException() { }
+
+        public DatosInvalidosException(string mensaje) : base(mensaje) { }
+
+        public DatosInvalidosException(string mensaje, Exception interna) : base(mensaje, interna) { }
+        
+    }
+}
---
+++ LogicaNegocio/EntidadesDominio/Tema.cs
@@ -0,0 +1,29 @@
+﻿using ExcepcionesPropias;
+using LogicaNegocio.InterfacesDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.EntidadesDominio
+{
+    internal class Tema : IValidable
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(this.Nombre))
+            {
+                throw new DatosInvalidosException("El Nombre del tema es Obligatorio");
+            }
+
+            if (this.Nombre.Length < 3 || this.Nombre.Length > 128)
+            {
+                throw new DatosInvalidosException("El Nombre del tema debe tener entre 3 y 128 caracteres");
+            }
+        }
+    }
+}
